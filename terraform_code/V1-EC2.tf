5fb8ed5be3a79170c62e767ac3e5c05a5c379611
VarunK1535 <varun.k1535@gmail.com>
2024-03-25 23:39:51 +0530

    v2-ec2 adding

--- terraform_code/V1-EC2.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-    region = "us-east-1"
-}
-
-resource "aws_instance" "demo-server" {
-    ami = "ami-02d7fd1c2af6eead0"
-    instance_type = "t2.micro"
-    key_name = "devops_project_key_vk1535"
-}
